bb2a6a5072d0222452ab64cd6ba3fd949d59038f
pankajchanana78 <pankaj_chanana@hotmail.com>
2025-02-01 08:13:53 +0530

    dd

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
 const config = {
-  project_id: '64884dd1255c301bd1ad',
-  database_id: '64884eb24476ed69928f',
+  project_id: '679912f8002479d02d2b',
+  database_id: '67991455000808c49623',
   collectionIds: {
-    states: '64887cd67bc409622368',
-    cities: '64887d6681c40f38330d',
+    states: '679d861c002314fd43f2',
+    cities: '679d862d003e405c4278',
     posts: '648885c4e0a3b566339e',
-    categories: '64884eff77e731d60741',
-    user_details: '64887f379b5be4bde451',
-    chats: '64887f902e7019837024',
-    messages: '64888017e5e54ab4ff28',
+    categories: '679d8649001b9d4bff11',
+    user_details: '679d8651000a2d926534',
+    chats: '679d865e001b6c075c0e',
+    messages: '679d86680034c188f6bf',
   },
   buckets: {
-    post_images: '64884e873050fbe97fe5',
+    post_images: '679d86c400147418d519',
   },
 };
 export default config;
